f1c5c8024d7ebf2ddc52de5213383f93a736fca6
git504 <git504@mailo.com>
2022-03-02 15:32:49 +0100

    Form Validation

---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+function CreatePost() {
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("title is needed !"),
+    postText: Yup.string().max(150).required("post is a required field"),
+    username: Yup.string()
+      .min(3)
+      .max(15)
+      .required("username is a required field"),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/posts", data).then((response) => {
+      console.log("it worked");
+    });
+  };
+
+  return (
+    <div className="createPostPage">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Title </label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="title"
+            placeholder="ex : Hey !"
+          />
+          <label>Post </label>
+          <ErrorMessage name="postText" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="postText"
+            placeholder="ex : Bla bla..."
+          />
+          <label>Username </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="username"
+            placeholder="ex : Rihanna"
+          />
+          <button type="submit">Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default CreatePost;
