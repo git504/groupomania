9e1cc10aa739f68793b672daf5a9ab63ba41d6b9
POP3 <76697214+git504@users.noreply.github.com>
2022-04-04 15:57:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ db.sequelize
   .sync()
   .then(() => {
     app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("server running");
 });
   })
   .catch((err) => {
